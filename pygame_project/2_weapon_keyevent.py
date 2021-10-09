9ffbafd3200ab6f2b10a047c372869a688f3f416
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2021-10-10 03:28:49 +0900

    pygame practice

--- pygame_project/2_weapon_keyevent.py
+++ pygame_project/2_weapon_keyevent.py
@@ -36,8 +36,22 @@ character_height = character_size[1]
 character_x_pos = (screen_width / 2) - (character_width / 2)
 character_y_pos = (screen_height) - (character_height) - (stage_height)
 
-# 이동 속도
+# 캐릭터 이동 방향
+character_to_x = 0
+
+# 캐릭터 이동 속도
 character_speed = 5
+
+# 무기 만들기
+weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]   # width만 필요한 이유: 캐릭터가 무기를 쏠 때 중앙에서 나옴 -> 중앙 처리 : 캐릭터 width/2 - 무기 width/2 한 위치 
+
+# 무기는 한 번에 여러 발 발사 가능
+weapons = []
+
+# 무기 이동 속도
+weapon_speed = 10
 
 # 이벤트 루프
 running = True   # 게임이 진행중인가?
@@ -50,22 +64,55 @@ while running:
         if event.type == pygame.QUIT:   # 창이 닫히는 이벤트가 발생하였는가?(pygame 창을 껐을 때 발생)
             running = False   # 게임이 진행중이 아님(while문 탈출)
 
-   
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:   # 캐릭터를 왼쪽으로
+                character_to_x -= character_speed
+
+            elif event.key == pygame.K_RIGHT:   # 캐릭터를 오른쪽으로
+                character_to_x += character_speed
+
+            elif event.key == pygame.K_SPACE:   # 무기 발사
+                weapon_x_pos = character_x_pos + (character_width / 2) - (weapon_width / 2)   # 무기를 발사한 위치 정의 - 플레이어의 위치 중앙
+                weapon_y_pos = character_y_pos
+                weapons.append([weapon_x_pos, weapon_y_pos])   # 무기를 여러 번 쏠 경우 (x,y) 값이 여러 개가 나올 것
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_to_x = 0
+
     # 3. 게임 캐릭터 위치 정의
-    # for문을 나와 실제  캐릭터 x, y 좌표 바꿔주기
-    # fps 값이 달라도 " 실제 게임 자체의 속도 "가 달라져서는 안 됨!! 
-    # -> 이동의 부드러운 정도는 달라지되, 이동 속도가 달라지면 안 되므로 보정 거치기
-    # 위치 좌표에 보정 처리
-    # 가로 경계값 처리
-    # 세로 경계값 처리
+    character_x_pos += character_to_x
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
    
+    # 무기 위치 조정
+    # 100, 200 -> 180, 160, 140, ...
+    # 500, 200 -> 180, 160, 140, ...
+    weapons = [ [w[0], w[1] - weapon_speed] for w in weapons]   # 무기 위치를 위로 올림
+
+    # 천장에 닿은 무기 없애기
+    weapons = [ [w[0], w[1]] for w in weapons if w[1] > 0]   # 천장에 닿지 않은 것에 대해서만 리스트로 . 
+
     # 4. 충돌 처리
     # 충돌 처리를 위한 rect 정보 업데이트
 
     # 5. 화면에 그리기
     screen.blit(background, (0,0))
+
+    for weapon_x_pos, weapon_y_pos in weapons:
+        screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
+
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
+
+    # 무기 위치에 해당하는 부분을 가장 아래 하면, 배경이 그려지고 -> 그 위에 stage가 그려지고 -> 캐릭터가 그려지고 -> 무기가 그려져
+    # 캐릭터 위를 무기가 지나가게됨. -> stage 위로 코드 위치 변경
+    # for weapon_x_pos, weapon_y_pos in weapons:
+    #     screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
+
 
     # 매 프레임마다 화면을 새로 그려주는 동작
     pygame.display.update()   # 게임 화면을 다시 그리기! (계속해서 호출이 되어야 함.)
